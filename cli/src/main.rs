b003cc59352efb6f1cdbbe17cda4976ac686726e
Craig McLure <craig@frostycoolslug.com>
2022-01-04 10:37:18 +0000

    Set Faders before Volume to prevent volume change interruption

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -43,15 +43,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut goxlr = GoXLR::open()?;
 
+    goxlr.set_fader(Fader::A, Channel::Mic)?;
+    goxlr.set_fader(Fader::B, Channel::Chat)?;
+    goxlr.set_fader(Fader::C, Channel::Music)?;
+    goxlr.set_fader(Fader::D, Channel::System)?;
+
     goxlr.set_volume(Channel::Mic, 0xFF)?;
     goxlr.set_volume(Channel::Chat, 0xFF)?;
     goxlr.set_volume(Channel::Music, 0xFF)?;
     goxlr.set_volume(Channel::System, 0xFF)?;
 
-    goxlr.set_fader(Fader::A, Channel::Mic)?;
-    goxlr.set_fader(Fader::B, Channel::Chat)?;
-    goxlr.set_fader(Fader::C, Channel::Music)?;
-    goxlr.set_fader(Fader::D, Channel::System)?;
 
     goxlr.set_channel_state(Channel::System, ChannelState::Unmuted);
 
