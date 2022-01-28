7b254db50e915c2ad6d9428b980f918d872a1e51
Nathan Adams <dinnerbone@dinnerbone.com>
2022-01-28 22:38:14 +0100

    Updated wireshark plugin to help decode effects

--- goxlr-wireshark-plugin.lua
+++ goxlr-wireshark-plugin.lua
@@ -1,6 +1,7 @@
 local COMMAND_NAMES = {
     [0x000] = "SystemInfo",
     [0x800] = "GetButtonStates",
+    [0x801] = "SetEffectParameters",
     [0x802] = "SetScribble",
     [0x803] = "SetColourMap",
     [0x804] = "SetRouting",
@@ -22,11 +23,14 @@ local f_header_subcommand = ProtoField.uint24("goxlr.header.subcommand", "Subcom
 local f_header_length = ProtoField.uint16("goxlr.header.length", "Body Length", base.DEC)
 local f_command_index = ProtoField.uint16("goxlr.header.index", "Index", base.DEC)
 local f_body = ProtoField.bytes("goxlr.body", "Body")
+local f_body_effect = ProtoField.bytes("goxlr.body", "Effect")
+local f_body_effect_key = ProtoField.uint16("goxlr.body.effect.key", "Effect Key", base.HEX)
+local f_body_effect_value = ProtoField.uint16("goxlr.body.effect.value", "Effect Value", base.HEX)
 
 local f_data_fragment = Field.new("usb.data_fragment")
 local f_control_response = Field.new("usb.control.Response")
 
-goxlr_protocol.fields = { f_header, f_header_command, f_header_subcommand, f_header_length, f_command_index, f_body }
+goxlr_protocol.fields = { f_header, f_header_command, f_header_subcommand, f_header_length, f_command_index, f_body, f_body_effect, f_body_effect_key, f_body_effect_value }
 
 function goxlr_protocol.dissector(buffer, pinfo, tree)
     data_fragment = f_data_fragment()
@@ -44,14 +48,26 @@ function goxlr_protocol.dissector(buffer, pinfo, tree)
 
     local subtree = tree:add(goxlr_protocol, buffer(), "GoXLR Command")
     local header = subtree:add(f_header, buffer(0, 16))
-    local command_id = buffer(0, 4):le_uint()
-    header:add_le(f_header_command, buffer(0, 4), bit.band(bit.rshift(command_id, 12), 0xfff))
-    header:add_le(f_header_subcommand, buffer(0, 4), bit.band(command_id, 0xfff))
+    local command = buffer(0, 4):le_uint()
+    local command_id = bit.band(bit.rshift(command, 12), 0xfff)
+    local subcommand_id = bit.band(command, 0xfff)
+    header:add_le(f_header_command, buffer(0, 4), command_id)
+    header:add_le(f_header_subcommand, buffer(0, 4), subcommand_id)
     header:add_le(f_header_length, buffer(4, 2))
     header:add_le(f_command_index, buffer(6, 2))
 
     if length > 16 then
-        subtree:add(f_body, buffer(16))
+        local body = subtree:add(f_body, buffer(16))
+        local body_buffer = buffer(16)
+
+        if command_id == 0x801 then
+            for i = 0, body_buffer:len() - 1, 8 do
+                local effect_buffer = body_buffer(i, 8)
+                local effect = body:add(f_body_effect, effect_buffer)
+                effect:add_le(f_body_effect_key, effect_buffer(0, 4))
+                effect:add_le(f_body_effect_value, effect_buffer(4, 4))
+            end
+        end
     end
 end
 
