b55cc37d94a8f9f1199a959822948a0e1ed80cc5
Nathan Adams <dinnerbone@dinnerbone.com>
2022-02-06 22:15:14 +0100

    Daemon will try to test each potential goxlr device connected

--- daemon/src/primary_worker.rs
+++ daemon/src/primary_worker.rs
@@ -2,12 +2,12 @@ use crate::device::Device;
 use crate::Shutdown;
 use anyhow::{anyhow, Result};
 use goxlr_ipc::{DaemonStatus, DeviceType, GoXLRCommand, HardwareStatus, UsbProductInformation};
-use goxlr_usb::goxlr;
-use goxlr_usb::goxlr::GoXLR;
-use goxlr_usb::rusb::GlobalContext;
+use goxlr_usb::goxlr::{GoXLR, PID_GOXLR_FULL, PID_GOXLR_MINI, VID_GOXLR};
+use goxlr_usb::rusb::{DeviceDescriptor, GlobalContext};
+use goxlr_usb::{goxlr, rusb};
 use log::{error, info};
 use std::collections::HashMap;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use tokio::sync::{mpsc, oneshot};
 use tokio::time::sleep;
 
@@ -22,21 +22,12 @@ pub type DeviceReceiver = mpsc::Receiver<DeviceCommand>;
 pub async fn handle_changes(mut rx: DeviceReceiver, mut shutdown: Shutdown) {
     let sleep_duration = Duration::from_millis(100);
     let mut devices = HashMap::new();
+    let mut ignore_list = HashMap::new();
 
     loop {
         tokio::select! {
             () = sleep(sleep_duration) => {
-                // TODO: Make GoXLR::open() take a specific hardware device
-                if devices.is_empty() {
-                    match load_new_device() {
-                        Ok(device) => {
-                            devices.insert(device.serial().to_owned(), device);
-                        },
-                        Err(e) => {
-                            error!("Error initializing device: {}", e);
-                        },
-                    }
-                }
+                find_new_device(&mut devices, &mut ignore_list);
                 for device in devices.values_mut() {
                     if let Err(e) = device.monitor_inputs() {
                         error!("Couldn't monitor device for inputs: {}", e);
@@ -69,8 +60,55 @@ pub async fn handle_changes(mut rx: DeviceReceiver, mut shutdown: Shutdown) {
     }
 }
 
-fn load_new_device() -> Result<Device<GlobalContext>> {
-    let mut device = GoXLR::open()?;
+fn find_new_device(
+    existing_devices: &mut HashMap<String, Device<GlobalContext>>,
+    devices_to_ignore: &mut HashMap<(u8, u8), Instant>,
+) {
+    let now = Instant::now();
+    if let Ok(devices) = rusb::devices() {
+        for device in devices.iter() {
+            if let Ok(descriptor) = device.device_descriptor() {
+                let bus_number = device.bus_number();
+                let address = device.address();
+                if descriptor.vendor_id() == VID_GOXLR
+                    && (descriptor.product_id() == PID_GOXLR_FULL
+                        || descriptor.product_id() == PID_GOXLR_MINI)
+                    && !existing_devices.values().any(|d| {
+                        d.status().hardware.usb_device.bus_number == bus_number
+                            && d.status().hardware.usb_device.address == address
+                    })
+                    && !devices_to_ignore
+                        .iter()
+                        .any(|((bus_number, address), expires)| {
+                            *bus_number == device.bus_number()
+                                && *address == device.address()
+                                && expires > &now
+                        })
+                {
+                    match load_device(device, descriptor) {
+                        Ok(device) => {
+                            existing_devices.insert(device.serial().to_owned(), device);
+                        }
+                        Err(e) => {
+                            error!(
+                                "Couldn't load potential GoXLR on bus {} address {}: {}",
+                                bus_number, address, e
+                            );
+                            devices_to_ignore
+                                .insert((bus_number, address), now + Duration::from_secs(10));
+                        }
+                    };
+                }
+            }
+        }
+    }
+}
+
+fn load_device(
+    device: rusb::Device<GlobalContext>,
+    descriptor: DeviceDescriptor,
+) -> Result<Device<GlobalContext>> {
+    let mut device = GoXLR::from_device(device.open()?, descriptor)?;
     let descriptor = device.usb_device_descriptor();
     let device_type = match descriptor.product_id() {
         goxlr::PID_GOXLR_FULL => DeviceType::Full,
