815afcce71068ca9f13725f7628f1a34f779ddd1
Nathan Adams <dinnerbone@dinnerbone.com>
2022-07-08 08:50:20 +0200

    Better error handling and feedback for goxlr initialiser

--- initialiser/src/main.rs
+++ initialiser/src/main.rs
@@ -1,7 +1,9 @@
+use anyhow::{Context, Error};
 use rusb::Direction::{In, Out};
 use rusb::Error::Pipe;
 use rusb::Recipient::Interface;
 use rusb::RequestType::{Class, Vendor};
+use rusb::{Device, DeviceHandle, GlobalContext, UsbContext};
 use std::time::Duration;
 
 pub const VID_GOXLR: u16 = 0x1220;
@@ -11,6 +13,15 @@ pub const PID_GOXLR_FULL: u16 = 0x8fe0;
 fn main() {
     println!("Checking for available GoXLR devices..");
     find_devices();
+}
+
+fn device_address<C: UsbContext>(device: &Device<C>) -> String {
+    format!(
+        "(bus {}, port {}, device {})",
+        device.bus_number(),
+        device.port_number(),
+        device.address()
+    )
 }
 
 fn find_devices() {
@@ -23,9 +34,12 @@ fn find_devices() {
                 {
                     match device.open() {
                         Ok(mut handle) => {
-                            println!("Found GoXLR Device at {:?}, checking state..", handle);
+                            println!(
+                                "Found GoXLR Device {}, checking state..",
+                                device_address(&device)
+                            );
 
-                            handle.set_active_configuration(1);
+                            let _ = handle.set_active_configuration(1);
 
                             // Send a single vendor command across, see what happens..
                             let request_type = rusb::request_type(Out, Vendor, Interface);
@@ -39,53 +53,74 @@ fn find_devices() {
                             );
 
                             if result == Err(Pipe) {
-                                println!("Device not initialised, preparing..");
-                                // The GoXLR is not initialised, we need to fix that..
-                                handle.set_auto_detach_kernel_driver(true);
-
-                                if handle.claim_interface(0).is_err() {
-                                    println!("Unable to claim, failed to initialise..");
-                                    continue;
+                                match initialize_device(&mut handle) {
+                                    Ok(()) => {
+                                        println!(
+                                            "Device {} successfully initialised",
+                                            device_address(&device)
+                                        );
+                                    }
+                                    Err(e) => {
+                                        eprintln!(
+                                            "Couldn't initialize device {}: {}",
+                                            device_address(&device),
+                                            e
+                                        );
+                                    }
                                 }
-
-                                // Activate the GoXLR Vendor interface..
-                                let request_type = rusb::request_type(In, Vendor, Interface);
-                                let mut buf = vec![0; 24];
-                                handle.read_control(
-                                    request_type,
-                                    0,
-                                    0,
-                                    0,
-                                    &mut buf,
-                                    Duration::from_secs(1),
-                                );
-
-                                // Now activate audio..
-                                let request_type = rusb::request_type(Out, Class, Interface);
-                                handle.write_control(
-                                    request_type,
-                                    1,
-                                    0x0100,
-                                    0x2900,
-                                    &[0x80, 0xbb, 0x00, 0x00],
-                                    Duration::from_secs(1),
-                                );
-                                handle.release_interface(0);
-
-                                // Trigger a reset on the device, to hard reload kernel drivers, and reinit audio..
-                                handle.reset();
-
-                                println!("Device Initialised");
                             } else {
-                                println!("Device already initialised");
+                                println!("Device {} already initialised", device_address(&device));
                             }
                         }
                         Err(e) => {
-                            println!("Unable to open the device.. {}", e);
+                            println!(
+                                "Unable to open the device {}.. {}",
+                                device_address(&device),
+                                e
+                            );
                         }
                     }
                 }
             }
         }
     }
+}
+
+fn initialize_device(handle: &mut DeviceHandle<GlobalContext>) -> Result<(), Error> {
+    println!("Device not initialised, preparing..");
+    // The GoXLR is not initialised, we need to fix that..
+    let _ = handle.set_auto_detach_kernel_driver(true);
+
+    handle
+        .claim_interface(0)
+        .context("Couldn't claim device interface")?;
+
+    // Activate the GoXLR Vendor interface..
+    let request_type = rusb::request_type(In, Vendor, Interface);
+    let mut buf = vec![0; 24];
+    handle
+        .read_control(request_type, 0, 0, 0, &mut buf, Duration::from_secs(1))
+        .context("Couldn't activate vendor interface")?;
+
+    // Now activate audio..
+    let request_type = rusb::request_type(Out, Class, Interface);
+    handle
+        .write_control(
+            request_type,
+            1,
+            0x0100,
+            0x2900,
+            &[0x80, 0xbb, 0x00, 0x00],
+            Duration::from_secs(1),
+        )
+        .context("Couldn't activate device audio")?;
+
+    handle
+        .release_interface(0)
+        .context("Couldn't release device interface")?;
+
+    // Trigger a reset on the device, to hard reload kernel drivers, and reinit audio..
+    handle.reset().context("Couldn't reset device")?;
+
+    Ok(())
 }
