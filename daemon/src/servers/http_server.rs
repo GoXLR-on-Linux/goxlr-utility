a6753fdafb9a24672b846ba0e61e8f71ce71cf7c
Craig McLure <craig@mclure.net>
2022-11-10 15:01:13 +0000

    Complete Migration to internal file handler

--- daemon/src/servers/http_server.rs
+++ daemon/src/servers/http_server.rs
@@ -5,7 +5,6 @@ use actix::{
     WrapFuture,
 };
 use actix_cors::Cors;
-use actix_plus_static_files::{build_hashmap_from_included_dir, include_dir, Dir, ResourceFiles};
 use actix_web::dev::ServerHandle;
 use actix_web::http::header::ContentType;
 use actix_web::middleware::Condition;
@@ -15,7 +14,8 @@ use actix_web_actors::ws;
 use actix_web_actors::ws::CloseCode;
 use anyhow::{anyhow, Result};
 use futures::lock::Mutex;
-use log::{debug, info, warn};
+use include_dir::{include_dir, Dir};
+use log::{info, warn};
 use mime_guess::MimeGuess;
 use tokio::sync::oneshot::Sender;
 
@@ -24,7 +24,7 @@ use goxlr_ipc::{DaemonRequest, DaemonResponse, DaemonStatus};
 use crate::primary_worker::DeviceSender;
 use crate::servers::server_packet::handle_packet;
 
-const WEB_CONTENT: Dir = include_dir!("./web-content/");
+const WEB_CONTENT: Dir = include_dir!("./daemon/web-content/");
 
 struct Websocket {
     sender: DeviceSender,
@@ -103,7 +103,6 @@ pub async fn launch_httpd(
     with_cors: bool,
 ) -> Result<()> {
     let server = HttpServer::new(move || {
-        let static_files = build_hashmap_from_included_dir(&WEB_CONTENT);
         let cors = Cors::default()
             .allowed_origin_fn(|origin, _req_head| {
                 origin.as_bytes().starts_with(b"http://127.0.0.1")
@@ -119,7 +118,6 @@ pub async fn launch_httpd(
             .service(get_devices)
             .service(websocket)
             .default_service(web::to(default))
-        //.service(ResourceFiles::new("/", static_files))
     })
     .bind(("127.0.0.1", port))?
     .run();
@@ -175,14 +173,16 @@ async fn get_devices(usb_mutex: Data<Mutex<DeviceSender>>) -> HttpResponse {
 }
 
 async fn default(req: HttpRequest) -> HttpResponse {
-    let path = req.path();
+    let path = if req.path() == "/" || req.path() == "" {
+        "/index.html"
+    } else {
+        req.path()
+    };
     let path_part = &path[1..path.len()];
-
     let file = WEB_CONTENT.get_file(path_part);
     if let Some(file) = file {
-        let mime_type = MimeGuess::from_path(req.path()).first_or_octet_stream();
+        let mime_type = MimeGuess::from_path(path).first_or_octet_stream();
         let mut builder = HttpResponse::Ok();
-
         builder.insert_header(ContentType(mime_type));
         builder.body(file.contents())
     } else {
