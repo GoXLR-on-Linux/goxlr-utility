6df12471d3f8562921d86fcfb294ea9963f37ab6
Craig McLure <craig@mclure.net>
2022-12-13 16:52:57 +0000

    [WIP] Tentative Initial Support for Tray Icons
    Probably best to not use this yet :p

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,12 +1,20 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use actix_web::dev::ServerHandle;
 use anyhow::{bail, Context, Result};
 use clap::Parser;
 use goxlr_ipc::HttpSettings;
 use json_patch::Patch;
-use log::{error, info, warn};
+use log::{debug, error, info, warn};
 use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use tokio::sync::{broadcast, mpsc};
 use tokio::{join, signal};
+use tray_icon::menu::{menu_event_receiver, Menu, MenuItem};
+use tray_icon::{tray_event_receiver, TrayIconBuilder};
+use winit::event_loop::EventLoopBuilder;
+use winit::platform::run_return::EventLoopExtRunReturn;
 
 use crate::cli::{Cli, LevelFilter};
 use crate::files::{get_file_paths_from_settings, run_notification_service, FileManager};
@@ -30,6 +38,8 @@ mod shutdown;
 // This can probably go somewhere else, but for now..
 const DISTRIBUTABLE_ROOT: &str = "/usr/share/goxlr/";
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const ICON: &[u8] = include_bytes!("../resources/goxlr-icon-dark.png");
 
 // This is for global 'JSON Patches', for when something changes.
 #[derive(Debug, Clone)]
@@ -138,9 +148,17 @@ async fn main() -> Result<()> {
         warn!("HTTP Server Disabled");
     }
 
-    await_ctrl_c(shutdown.clone()).await;
+    // Create non-async method of shutting down threads..
+    let blocking_shutdown = Arc::new(AtomicBool::new(false));
+
+    // Setup Ctrl+C Monitoring..
+    tokio::spawn(await_ctrl_c(shutdown.clone(), blocking_shutdown.clone()));
+
+    // Spawn the Systray Icon + Menu.. This will block until Ctrl+C is received.
+    spawn_menu(blocking_shutdown.clone())?;
 
     info!("Shutting down daemon");
+
     if let Some(server) = http_server {
         // We only need to Join on the HTTP Server if it exists..
         let _ = join!(
@@ -157,9 +175,10 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn await_ctrl_c(shutdown: Shutdown) {
+async fn await_ctrl_c(shutdown: Shutdown, blocking_shutdown: Arc<AtomicBool>) {
     if signal::ctrl_c().await.is_ok() {
         shutdown.trigger();
+        blocking_shutdown.store(true, Ordering::Relaxed);
     }
 }
 
@@ -172,4 +191,62 @@ fn is_root() -> bool {
 fn is_root() -> bool {
     // On non-unix systems, we can't root check, assume we're good!
     false
+}
+
+fn spawn_menu(shutdown: Arc<AtomicBool>) -> Result<()> {
+    let tray_menu = Menu::new();
+    let hello_menu = MenuItem::new("Hello", true, None);
+    tray_menu.append_items(&[&hello_menu]);
+
+    let tray_icon = TrayIconBuilder::new()
+        .with_menu(Box::new(tray_menu))
+        .with_tooltip("GoXLR Utility")
+        .with_icon(load_icon())
+        .build()?;
+
+    let tray_channel = tray_event_receiver();
+    let menu_channel = menu_event_receiver();
+
+    // So the problem is, on certain OSs, the Event Loop handler *HAS* to be handled on
+    // the main thread. So this is a blocking call. We'll keep an eye out for the shutdown
+    // handle being changed, so we can exit gracefully when Ctrl+C is hit.
+    let mut event_loop = EventLoopBuilder::new().build();
+    event_loop.run_return(move |_event, _, control_flow| {
+        // We set this to poll, so we can monitor both the menu, and tray icon..
+        control_flow.set_poll();
+
+        if let Ok(event) = menu_channel.try_recv() {
+            if event.id == hello_menu.id() {
+                debug!("Hello Button Pressed! :)");
+            }
+            debug!("{:?}", event);
+        }
+
+        if let Ok(event) = tray_channel.try_recv() {
+            debug!("{:?}", event);
+        }
+
+        if shutdown.load(Ordering::Relaxed) {
+            debug!("Shutting down Window Event Handler..");
+            control_flow.set_exit();
+        }
+    });
+
+    // When we get here we're done with the event listener. We need to drop the tray icon
+    // to ensure any 'background' cleanup is done.
+    drop(tray_icon);
+    Ok(())
+}
+
+fn load_icon() -> tray_icon::icon::Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::load_from_memory(ICON)
+            .expect("Failed to load Icon")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    tray_icon::icon::Icon::from_rgba(icon_rgba, icon_width, icon_height)
+        .expect("Failed to load Icon")
 }
