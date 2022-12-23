694f531db7cc51080cefbfc31ed74789b68e53c1
Craig McLure <craig@mclure.net>
2022-12-23 06:21:30 +0000

    renamed SVG file to match other icons

--- daemon/resources/create-icons.sh
+++ daemon/resources/create-icons.sh
@@ -6,7 +6,7 @@ mkdir tmp/
 FILE_PATHS=""
 for size in 16 32 48 128 256; do
   FILE_NAME="tmp/$size.png"
-  inkscape GoXLR.svg --export-filename=$FILE_NAME -w $size -h $size
+  inkscape goxlr-utility.svg --export-filename=$FILE_NAME -w $size -h $size
   FILE_PATHS="${FILE_PATHS} ${FILE_NAME}"
 done
 convert $FILE_PATHS goxlr-utility.ico
@@ -15,7 +15,6 @@ rm -r tmp/
 
 # On Linux, we need to add a 20px margin..
 # left -25, top -25, right: 281.0, bottom: 281.0 width:306, height: 306
-# inkscape ../GoXLR.svg --export-filename=$size-png.png -w $size -h $size --export-area=-25:-25:281:281
 
 # For Linux, we need to add a margin to the icon so it looks clean in things like system trays, we'll use around 10%
 # We need:
@@ -23,8 +22,8 @@ rm -r tmp/
 # 48x48 PNG in /usr/share/icons/hicolor/48x48/apps/
 # SVG to /usr/share/icons/hicolor/scalable/apps/
 # And a 128x128 png for embedding
-inkscape GoXLR.svg --export-filename=goxlr-utility-large.png -w 128 -h 128 --export-area=-25:-25:281:281
-inkscape GoXLR.svg --export-filename=goxlr-utility.png -w 48 -h 48 --export-area=-25:-25:281:281
-inkscape GoXLR.svg --export-filename=tmp.png -w 32 -h 32 --export-area=-25:-25:281:281
+inkscape goxlr-utility.svg --export-filename=goxlr-utility-large.png -w 128 -h 128 --export-area=-25:-25:281:281
+inkscape goxlr-utility.svg --export-filename=goxlr-utility.png -w 48 -h 48 --export-area=-25:-25:281:281
+inkscape goxlr-utility.svg --export-filename=tmp.png -w 32 -h 32 --export-area=-25:-25:281:281
 convert tmp.png goxlr-utility.xpm
 rm tmp.png
