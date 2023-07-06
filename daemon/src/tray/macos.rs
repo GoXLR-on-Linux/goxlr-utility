a3592dc922ee76775c0893992c00fc703186770a
Craig McLure <craig@mclure.net>
2023-07-06 05:54:48 +0100

    Replaced the Tray with a bespoke solution

--- daemon/src/tray/macos.rs
+++ daemon/src/tray/macos.rs
@@ -19,48 +19,16 @@ use tao::system_tray::SystemTrayBuilder;
 use tao::TrayId;
 use tokio::sync::mpsc::Sender;
 
+use crate::tray::get_icon;
 use goxlr_ipc::PathTypes::{Icons, MicProfiles, Presets, Profiles, Samples};
-#[cfg(target_os = "macos")]
 use tao::platform::macos::{ActivationPolicy, EventLoopExtMacOS};
-
-cfg_if! {
-    if #[cfg(windows)] {
-        use std::thread;
-        use win_win::{WindowBuilder, WindowClass, WindowProc};
-        use winapi::um::winuser::{ShowWindow, SW_HIDE};
-        use std::ptr::null_mut;
-    }
-}
 
 pub fn handle_tray(state: DaemonState, tx: Sender<EventTriggers>) -> Result<()> {
     let mut event_loop = EventLoop::new();
+    event_loop.set_activation_policy(ActivationPolicy::Accessory);
+    event_loop.set_activate_ignoring_other_apps(true);
 
-    #[cfg(target_os = "macos")]
-    {
-        event_loop.set_activation_policy(ActivationPolicy::Accessory);
-        event_loop.set_activate_ignoring_other_apps(true);
-    }
-
-    let ready = Arc::new(AtomicBool::new(false));
-    #[cfg(windows)]
-    {
-        let ready_inner = ready.clone();
-        let win_state = state.clone();
-        let win_global = tx.clone();
-        thread::spawn(move || create_window(win_state, win_global, ready_inner));
-    }
-
-    #[cfg(not(windows))]
-    {
-        ready.store(true, Ordering::Relaxed);
-    }
-
-    // Ok, we need to loop here until the main window is ready..
-    while !ready.load(Ordering::Relaxed) {
-        sleep(Duration::from_millis(5));
-    }
     debug!("Launching Tray..");
-
     let tray_id = TrayId::new("goxlr-utility-tray");
     let icon = load_icon();
 
@@ -126,12 +94,6 @@ pub fn handle_tray(state: DaemonState, tx: Sender<EventTriggers>) -> Result<()> 
                     let _ = tx.try_send(EventTriggers::Open(Icons));
                 }
             }
-            Event::TrayEvent { event, .. } => {
-                // Left click on Mac opens the menu, so we don't want to trigger this.
-                if event == LeftClick && cfg!(not(target_os = "macos")) {
-                    let _ = tx.try_send(EventTriggers::Activate);
-                }
-            }
             _ => {}
         }
 
@@ -145,89 +107,7 @@ pub fn handle_tray(state: DaemonState, tx: Sender<EventTriggers>) -> Result<()> 
 }
 
 fn load_icon() -> tao::system_tray::Icon {
-    let (icon_rgba, icon_width, icon_height) = {
-        let image = image::load_from_memory(ICON)
-            .expect("Failed to load Icon")
-            .into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        (rgba, width, height)
-    };
+    let (icon_rgba, icon_width, icon_height) = get_icon_from_global();
     tao::system_tray::Icon::from_rgba(icon_rgba, icon_width, icon_height)
         .expect("Failed to load Icon")
-}
-
-#[cfg(windows)]
-fn create_window(state: DaemonState, tx: Sender<EventTriggers>, ready: Arc<AtomicBool>) {
-    let win_class = WindowClass::builder("goxlr-utility").build().unwrap();
-    let window_proc = GoXLRWindowProc::new(state, tx, ready);
-    let hwnd = WindowBuilder::new(window_proc, &win_class).build();
-
-    unsafe {
-        ShowWindow(hwnd, SW_HIDE);
-        win_win::runloop(null_mut());
-    }
-}
-
-#[cfg(windows)]
-struct GoXLRWindowProc {
-    state: DaemonState,
-    global_tx: Sender<EventTriggers>,
-    ready: Arc<AtomicBool>,
-}
-
-#[cfg(windows)]
-impl GoXLRWindowProc {
-    pub fn new(state: DaemonState, tx: Sender<EventTriggers>, ready: Arc<AtomicBool>) -> Self {
-        Self {
-            state,
-            global_tx: tx,
-            ready,
-        }
-    }
-}
-
-#[cfg(windows)]
-impl WindowProc for GoXLRWindowProc {
-    fn window_proc(
-        &self,
-        _hwnd: winapi::shared::windef::HWND,
-        msg: winapi::shared::minwindef::UINT,
-        _wparam: winapi::shared::minwindef::WPARAM,
-        _lparam: winapi::shared::minwindef::LPARAM,
-    ) -> Option<winapi::shared::minwindef::LRESULT> {
-        match msg {
-            winapi::um::winuser::WM_CREATE => {
-                // Window has spawned, allow other code to continue.
-                debug!("Window Spawned, resuming startup..");
-                self.ready.store(true, Ordering::Relaxed);
-            }
-            winapi::um::winuser::WM_QUERYENDSESSION => {
-                // This will fall through and default to 'True'
-                debug!("Query End Session Received..");
-            }
-            winapi::um::winuser::WM_ENDSESSION => {
-                debug!("Received WM_ENDSESSION from Windows");
-
-                // Ok, Windows has prompted an session closure here, we need to make sure the
-                // daemon shuts down correctly..
-                debug!("Attempting Shutdown..");
-                let _ = self.global_tx.try_send(EventTriggers::Stop);
-
-                // Now wait for the daemon to actually stop..
-                loop {
-                    if self.state.shutdown_blocking.load(Ordering::Relaxed) {
-                        break;
-                    } else {
-                        debug!("Waiting..");
-                        sleep(Duration::from_millis(100));
-                    }
-                }
-
-                debug!("Shutdown Complete?");
-            }
-            _ => {}
-        }
-        None
-    }
 }
