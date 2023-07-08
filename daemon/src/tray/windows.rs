a820656108f472c283f6eeb27ece4dd92718058b
Craig McLure <craig@mclure.net>
2023-07-08 04:15:09 +0100

    Shutdown if the hidden window is closed

--- daemon/src/tray/windows.rs
+++ daemon/src/tray/windows.rs
@@ -255,9 +255,9 @@ impl WindowProc for GoXLRWindowProc {
 
             // Window Handler
             winuser::WM_CLOSE => {
-                // Prevent Windows from Closing this window..
-                debug!("WINDOWS TRYING TO CLOSE US!");
-                return Some(1);
+                // If something tries to close this hidden window, it's a good bet that it wants
+                // us to shutdown.
+                let _ = self.global_tx.try_send(EventTriggers::Stop);
             }
 
             // // Shutdown Handlers..
