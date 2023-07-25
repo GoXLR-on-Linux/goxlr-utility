872708837002d2c3c2e1a1c83aa6711becbda05b
Craig McLure <craig@mclure.net>
2023-07-25 23:20:52 +0100

    Skip Pause not needed on Linux

--- usb/src/device/libusb/device.rs
+++ usb/src/device/libusb/device.rs
@@ -142,7 +142,7 @@ impl AttachGoXLR for GoXLRUSB {
         device: GoXLRDevice,
         disconnect_sender: Sender<String>,
         event_sender: Sender<String>,
-        skip_pause: bool,
+        _skip_pause: bool,
     ) -> Result<Box<(dyn FullGoXLRDevice)>> {
         // Firstly, we need to locate the USB device based on the location..
         let (device, descriptor) = GoXLRUSB::find_device(device)?;
