2d0cd1d4c32c17f2b7c221f77c04f14bf1ff934c
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2025-03-20 15:55:43 +0100

    fix: rename `icons.icns` to `icon.icns`

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -59,7 +59,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[allow(dead_code)]
 const ICON: &[u8] = include_bytes!("../resources/goxlr-utility-large.png");
 #[cfg(target_os = "macos")]
-const ICON_MAC: &[u8] = include_bytes!("../resources/icons.icns");
+const ICON_MAC: &[u8] = include_bytes!("../resources/icon.icns");
 
 const FIRMWARE_PATHS: EnumMap<FirmwareSource, &str> = EnumMap::from_array([
     "https://mediadl.musictribe.com/media/PLM/sftp/incoming/hybris/import/GOXLR/",
