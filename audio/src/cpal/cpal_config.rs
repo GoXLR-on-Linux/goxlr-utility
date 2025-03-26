f75080746e238a753f8199a47f5c39a7bc108ba3
Craig McLure <craig@mclure.net>
2025-03-26 05:37:34 +0000

    Another attempt to fix Windows audio performance

--- audio/src/cpal/cpal_config.rs
+++ audio/src/cpal/cpal_config.rs
@@ -5,6 +5,64 @@ use cpal::Device;
 pub struct CpalConfiguration {}
 
 impl CpalConfiguration {
+    #[cfg(target_os = "windows")]
+    pub(crate) fn get_device(device: Option<String>, input: bool) -> Result<Device> {
+        // Under Windows, we just want a device, we don't care about its formats, it's either
+        // and input device, or an output device, And we can match that by the bool.
+        let mut cpal_device = None;
+
+        // The Goal here is to find a device by host, and name, Nothing else.
+        if let Some(device_name) = device {
+            if let Some(position) = device_name.find('*') {
+                let str_host = &device_name[0..position];
+                let str_device = &device_name[position + 1..device_name.len()];
+
+                // Grab the Correct host...
+                let cpal_host_list = cpal::available_hosts();
+                let host_id = cpal_host_list.iter().find(|x| x.name() == str_host);
+
+                if let Some(host_id) = host_id {
+                    if let Ok(host) = cpal::host_from_id(*host_id) {
+                        // Check the relevant input and output group for the device
+                        cpal_device = if input {
+                            if let Ok(mut devices) = host.input_devices() {
+                                devices.find(|x| {
+                                    x.name().unwrap_or_else(|_| "UNKNOWN".to_string()) == str_device
+                                })
+                            } else {
+                                None
+                            }
+                        } else if let Ok(mut devices) = host.output_devices() {
+                            devices.find(|x| {
+                                x.name().unwrap_or_else(|_| "UNKNOWN".to_string()) == str_device
+                            })
+                        } else {
+                            None
+                        }
+                    };
+                }
+            }
+        }
+
+        if let Some(device) = cpal_device {
+            Ok(device)
+        } else {
+            let host = cpal::default_host();
+            let default_device = if input {
+                host.default_input_device()
+            } else {
+                host.default_output_device()
+            };
+
+            match default_device {
+                Some(device) => Ok(device),
+                None => bail!("Unable to find Default Device"),
+            }
+        }
+    }
+
+    // We'll CFG NOT WINDOWS here, this file isn't included on Linux, so this implies MacOS
+    #[cfg(not(target_os = "windows"))]
     pub(crate) fn get_device(device: Option<String>, input: bool) -> Result<Device> {
         let mut cpal_device = None;
 
